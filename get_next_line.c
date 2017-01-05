86983d1b12b13bfbe7d1d946ef06d98b25f2c42d
Anton Anatolievich <wartoxy@gmail.com>
2017-01-05 21:55:36 +0200

    trying to make it work with multiple file descriptors

--- get_next_line.c
+++ get_next_line.c
@@ -5,10 +5,34 @@
 #include "get_next_line.h"
 #include "libft.h"
 
+t_fd    *get_fd(t_fd *list, int fd)
+{
+    if (list)
+    {
+        while (list)
+        {
+            if (list->fd == fd)
+                return (list);
+            list = list->next;
+        }
+    } else
+    {
+        list = (t_fd*)malloc(sizeof(t_fd));
+        if (list)
+        {
+            list->fd = fd;
+            list->next = NULL;
+            return (list);
+        }
+    }
+    return (NULL);
+}
+
 int get_next_line(const int fd, char **line)
 {
-	static char buf[BUFF_SIZE + 1];
-	char		*result;
+	static t_fd *list;
+    t_fd        *elem;
+
 	char		*temp;
 	ssize_t		len;
 	size_t		temp_size;
@@ -16,94 +40,82 @@ int get_next_line(const int fd, char **line)
 
 	bytes_read = 0;
 	temp_size = 0;
-	result = NULL;
+	*line = NULL;
 
 	//подточить напильником логику так, чтобы возвращало -1
 	//если одна из библиотечных функций не смогла выделить память.
-	if (fd < 0)
-		return (-1);
-	/*
-	 ** тут пролистывать по статической структуре, ища нужный файл дескриптор.
-	 **
-	 ** в структуре можно на вяский случай сохранять индекс на котором начинаются оставшиеся символы
-	 ** и колличечтво считанных байт(длинну буфера)
-	 **
-	 ** после считывания остатка из буфера его даже не нужно фришить, это делает read
-	 */
+    if (fd < 0 || line == NULL || BUFF_SIZE <= 0 || !(elem = get_fd(list, fd)))
+        return (-1);
 
 	//если в буфере осталось что-то с прошлого раза
-	if (*buf)
+	if (*elem->buf)
 	{
 		/*
 		 **1)finding length of the line
 		 **2)allocateing memory for line, which will be returned
-		 **3)writing into result bytes from buffer
+		 **3)writing into *line bytes from buffer
 		 **4)copying remaining cymbols into temporary line
 		 **5)writing zeroes into old buffer
 		 **6)writing remain symbols into buffer
 		 **7)releasing temp
-		 **8)expanding size of result in case we will read from file again
+		 **8)expanding size of *line in case we will read from file again
 		 */
 		len = 0;
-		while (buf[len] != '\n' && buf[len])
+		while (elem->buf[len] != '\n' && elem->buf[len])
 			len++;
-		result = ft_strnew(len);
-		result = ft_strncpy(result, buf, len);
-		temp = ft_strdup(buf + len + 1);
-		ft_bzero(buf, BUFF_SIZE + 1);
-		ft_strcpy(buf, temp);
+        //if len == 0 ???
+        *line = ft_strnew(len);
+        *line = ft_strncpy(*line, elem->buf, len);
+		temp = ft_strdup(elem->buf + len + 1);
+		ft_bzero(elem->buf, BUFF_SIZE + 1);
+		ft_strcpy(elem->buf, temp);
 		free(temp);
 		temp_size += len;
 	}
 
-	while ((bytes_read = read(fd, buf, BUFF_SIZE)))
+	while ((bytes_read = read(fd, elem->buf, BUFF_SIZE)))
 	{
-		buf[bytes_read] = '\0';
+        if (bytes_read == -1)
+            return (-1);
+		elem->buf[bytes_read] = '\0';
 		len = 0;
-		while (buf[len] != '\n' && len < bytes_read)
+		while (elem->buf[len] != '\n' && len < bytes_read)
 			len++;
 		temp_size += len;
-		//if it is a first step
-		if (!result)
+		if (!*line)
 		{
-			//create new null-terminated string
-			result = ft_strnew(len);
-			//copy buffer without \n into result
-			//may be strncpy doesn't suit, because it doesn't scroll the buffer pointer for the next step
-			result = ft_strncpy(result, buf, len);
+            *line = ft_strnew(len);
+            *line = ft_strncpy(*line, elem->buf, len);
 		}
 		else
 		{
 			/*
-			 **1)create temporary copy of result
-			 **2)free old result
-			 **3)create enough space for result + buf
-			 **4)copy temp into result
-			 **5)concatanate buf into result
+			 **1)create temporary copy of *line
+			 **2)free old *line
+			 **3)create enough space for *line + elem->buf
+			 **4)copy temp into *line
+			 **5)concatanate elem->buf into *line
 			 **6)free temporary string
 			 */
-			temp = ft_strdup(result);
-			free(result);
-			result = ft_strnew(temp_size);
-			result = ft_strcpy(result, temp);
-			result = ft_strncat(result, buf, len);
+			temp = ft_strdup(*line);
+			free(*line);
+            *line = ft_strnew(temp_size);
+			*line = ft_strcpy(*line, temp);
+			*line = ft_strncat(*line, elem->buf, len);
 			free(temp);
 		}
 		if (len < bytes_read)
 		{
-			temp = ft_strdup(buf + len + 1);
-			ft_bzero(buf, BUFF_SIZE + 1);
-			ft_strcpy(buf, temp);
-			*line = result;
+			temp = ft_strdup(elem->buf + len + 1);
+			ft_bzero(elem->buf, BUFF_SIZE + 1);
+			ft_strcpy(elem->buf, temp);
+			*line = *line;
 			return(1);
 		}
 		if (len == bytes_read)
-			ft_bzero(buf, BUFF_SIZE + 1);
+			ft_bzero(elem->buf, BUFF_SIZE + 1);
 	}
-	if (result)
-	{
-		*line = result;
-		return (1);
-	}
-	return (0);
+	if (!*line)
+        return (0);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -14,4 +14,11 @@ int get_next_line(const int fd, char **line);
 
 #define BUFF_SIZE 10000000
 
+typedef struct      s_fd
+{
+    int             fd;
+    char            buf[BUFF_SIZE + 1];
+    struct s_fd *next;
+}                   t_fd;
+
 #endif //GET_NEXT_LINE_GET_NEXT_LINE_H
