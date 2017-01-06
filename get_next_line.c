d31fe7e89bc963a465bc39698827c48b2a9088f2
Anton Anatolievich <wartoxy@gmail.com>
2017-01-06 18:24:10 +0200

    it works, now little better but still big & ugly

--- get_next_line.c
+++ get_next_line.c
@@ -5,127 +5,91 @@
 #include "get_next_line.h"
 #include "libft.h"
 
-t_fd *get_fd(t_fd **desc, int fd)
+t_fd *get_fd(t_fd *list, int fd)
 {
-	t_fd *ptr;
-
-	ptr = *desc;
-	if (ptr)
+	while (list)
 	{
-		while (ptr)
+		if (list->fd == fd)
+			return (list);
+		if (list->next)
+			list = list->next;
+		else if ((list->next = (t_fd *) malloc(sizeof(t_fd))))
 		{
-			if (ptr->fd == fd)
-				return (ptr);
-			if (ptr->next)
-				ptr = ptr->next;
-			else
-				break;
-		}
-		ptr->next = (t_fd *) malloc(sizeof(t_fd));
-		if (ptr->next)
-		{
-			ptr = ptr->next;
-			ptr->fd = fd;
-			ptr->next = NULL;
-			return (ptr);
+			list = list->next;
+			list->fd = fd;
+			list->next = NULL;
+			return (list);
 		}
 	}
-	ptr = (t_fd *) malloc(sizeof(t_fd));
-	if (ptr)
+	if ((list = (t_fd *) malloc(sizeof(t_fd))))
 	{
-		ptr->fd = fd;
-		ptr->next = NULL;
-		return (ptr);
+		list->fd = fd;
+		list->next = NULL;
+		return (list);
 	}
 	return (NULL);
 }
 
+void move_buf(char *elem_buf, int len)
+{
+	char *temp;
+
+	temp = ft_strdup(elem_buf + len + 1);
+	ft_bzero(elem_buf, BUFF_SIZE + 1);
+	ft_strcpy(elem_buf, temp);
+	free(temp);
+}
+
 int get_next_line(const int fd, char **line)
 {
-	static t_fd *desc;
+	static t_fd *list;
 	t_fd *elem;
 
 	char *temp;
 	ssize_t len;
-	size_t temp_size;
 	ssize_t bytes_read;
 
 	bytes_read = 0;
-	temp_size = 0;
 	*line = NULL;
-
-	//подточить напильником логику так, чтобы возвращало -1
-	//если одна из библиотечных функций не смогла выделить память.
 	if (fd < 0 || line == NULL || BUFF_SIZE <= 0)
 		return (-1);
-	if (desc)
-		elem = get_fd(&desc, fd);
+	if (list)
+		elem = get_fd(list, fd);
 	else
 	{
-		desc = get_fd(&desc, fd);
-		elem = desc;
+		list = get_fd(list, fd);
+		elem = list;
 	}
-
-	//если в буфере осталось что-то с прошлого раза
 	if (*elem->buf)
 	{
-		/*
-		 **1)finding length of the line
-		 **2)allocateing memory for line, which will be returned
-		 **3)writing into *line bytes from buffer
-		 **4)copying remaining cymbols into temporary line
-		 **5)writing zeroes into old buffer
-		 **6)writing remain symbols into buffer
-		 **7)releasing temp
-		 **8)expanding size of *line in case we will read from file again
-		 */
 		len = 0;
 		while (elem->buf[len] != '\n' && elem->buf[len])
 			len++;
-		//if len == 0 ???
 		*line = ft_strnew(len);
 		*line = ft_strncpy(*line, elem->buf, len);
-		temp = ft_strdup(elem->buf + len + 1);
-		ft_bzero(elem->buf, BUFF_SIZE + 1);
-		ft_strcpy(elem->buf, temp);
-		free(temp);
-		temp_size += len;
+		move_buf(elem->buf, len);
 	}
 	while ((bytes_read = read(fd, elem->buf, BUFF_SIZE)))
 	{
-		if (bytes_read == -1)
-			return (-1);
-		//elem->buf[bytes_read] = '\0';
 		len = 0;
-		while (elem->buf[len] != '\n' && len < bytes_read)
+		while (elem->buf[len] != '\n' && elem->buf[len])
 			len++;
-		temp_size += len;
 		if (!*line)
 		{
 			*line = ft_strnew(len);
 			*line = ft_strncpy(*line, elem->buf, len);
 		} else
 		{
-			/*
-			 **1)create temporary copy of *line
-			 **2)free old *line
-			 **3)create enough space for *line + elem->buf
-			 **4)copy temp into *line
-			 **5)concatanate elem->buf into *line
-			 **6)free temporary string
-			 */
 			temp = ft_strdup(*line);
 			free(*line);
-			*line = ft_strnew(temp_size);
-			*line = ft_strcpy(*line, temp);
+			*line = ft_strnew(ft_strlen(temp) + len);
+			ft_strcpy(*line, temp);
 			*line = ft_strncat(*line, elem->buf, len);
 			free(temp);
 		}
 		if (len < bytes_read)
 		{
-			temp = ft_strdup(elem->buf + len + 1);
-			ft_bzero(elem->buf, BUFF_SIZE + 1);
-			ft_strcpy(elem->buf, temp);
+			move_buf(elem->buf, len);
 			return (1);
 		}
 		if (len == bytes_read)
